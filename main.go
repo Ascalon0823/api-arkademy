db13908b3997a325b772d76ba686b880bd3ac474
Arkademy <ascalon0823@gmail.com>
2025-02-26 17:09:49 +0800

    add basic user login and get user use token

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -20,7 +24,7 @@ var (
 	jwtKey         = []byte(os.Getenv("JWT_SECRET"))
 )
 
-func ConnectDB() *mongo.Client {
+func connectDB() *mongo.Client {
 	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI_LOCAL"))
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
@@ -36,18 +40,50 @@ func ConnectDB() *mongo.Client {
 	return client
 }
 
-func HashPassword(password string) string {
+func hashPassword(password string) string {
 	hash := sha256.Sum256([]byte(password))
 	return hex.EncodeToString(hash[:])
 }
 
-func RegisterUser(c *gin.Context) {
+func generateToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 72).Unix(),
+	})
+	return token.SignedString(jwtKey)
+}
+
+func loginUser(c *gin.Context) {
 	var user User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	user.Password = HashPassword(user.Password)
+	var dbUser User
+	err := userCollection.FindOne(context.TODO(), bson.M{"username": user.Username}).Decode(&dbUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid username or password"})
+		return
+	}
+	if hashPassword(user.Password) != dbUser.Password {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid username or password"})
+		return
+	}
+	token, err := generateToken(user.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+func registerUser(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user.Password = hashPassword(user.Password)
 	_, err := userCollection.InsertOne(context.TODO(), user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
@@ -55,15 +91,54 @@ func RegisterUser(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
 }
+
+func authenticateToken(c *gin.Context) (*jwt.Token, error) {
+	tokenString := c.GetHeader("Authorization")
+	if tokenString == "" {
+		return nil, errors.New("Authorization header is required")
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("Invalid signing method")
+		}
+		return jwtKey, nil
+	})
+	return token, err
+}
+
+func getUser(c *gin.Context) {
+	token, err := authenticateToken(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		return
+	}
+	username := claims["username"].(string)
+	var user User
+	err = userCollection.FindOne(context.TODO(), bson.M{"username": username}).Decode(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "User not found"})
+		return
+	}
+	user.Password = ""
+	c.JSON(http.StatusOK, user)
+}
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading Environment")
 	}
-	client = ConnectDB()
+	client = connectDB()
 	defer client.Disconnect(context.Background())
 	router := gin.Default()
 
-	router.POST("/register", RegisterUser)
+	router.GET("/user", getUser)
+	router.POST("/register", registerUser)
+	router.POST("/login", loginUser)
 	router.Run(":8080")
 }
