f4dd658bf4cef70c9179c625d32485701bc91457
Arkademy <ascalon0823@gmail.com>
2025-02-26 15:41:07 +0800

    add mongo db interaction

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import ("fmt")
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Sale struct {
+	Item     string  `bson:"item"`
+	Price    float64 `bson:"price"`
+	Quantity int     `bson:"quantity"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading Environment")
+	}
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI_LOCAL"))
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	var results []Sale
+	coll := client.Database("mongodbVSCodePlaygroundDB").Collection("sales")
+	cursor, err := coll.Find(context.TODO(), bson.D{})
+	if err != nil {
+		panic(err)
+	}
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	for _, result := range results {
+		fmt.Printf("%+v\n", result)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
