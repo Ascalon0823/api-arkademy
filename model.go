46aac561d53467b4288ff58900d26e15289fd1a4
Dust <ascalon0823@Gmail.com>
2025-02-27 12:44:07 +0800

    update model

--- model.go
+++ model.go
@@ -3,7 +3,18 @@ package main
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Username string             `bson:"username"`
-	Password string             `bson:"password"`
+	ID           primitive.ObjectID `bson:"_id,omitempty"`
+	Username     string             `bson:"username"`
+	Password     string             `bson:"password"`
+	PlayerRecord PlayerRecord       `bson:"playerRecord"`
+}
+
+type PlayerRecord struct {
+	CreationTime   primitive.DateTime
+	LastPlayedTime primitive.DateTime
+	Characters     []CharacterRecord
+}
+
+type CharacterRecord struct {
+	DisplayName string
 }
